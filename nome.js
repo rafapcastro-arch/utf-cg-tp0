1ff2a99ba7b461c6b04574db52397b83911f81c0
rafapcastro-arch <rafapcastro@terra.com.br>
2026-08-19 04:22:18 -0300

    Update nome.js

--- nome.js
+++ nome.js
@@ -1,5 +1,5 @@
 const botaodlc = document.getElementById("botaodlc");
 
 botaodlc.addEventListener("click", function() {
-    alert('Espero que esse botão bata o dedinho na quina');
+    Windows.alert('Espero que esse botão bata o dedinho na quina');
 });
